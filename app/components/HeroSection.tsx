504a2a252353368a602dcc13570059d5a41156c7
Frederico Ramos <frederico.jlr@gmail.com>
2024-06-20 14:46:59 +0100

    hero section - form

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,8 +1,14 @@
 // REACT
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 // LIBRARIES
+import dayjs, { Dayjs } from "dayjs";
+import { Field, Form, Formik, FormikHelpers } from "formik";
 import Image from "next/image";
+import { Button, DateRangePicker, Select, SelectItem } from "@nextui-org/react";
+import { CalendarDate, getLocalTimeZone, today } from "@internationalized/date";
+
+// Swiper
 import { Autoplay, EffectFade, Navigation } from "swiper/modules";
 // import function to register Swiper custom elements
 import { register } from "swiper/element/bundle";
@@ -40,6 +46,42 @@ const travels = [
   { travelName: "Marrocos", rnavtRef: "RNAVT 1234", imgSrc: MarrocosCardImage, price: 598 },
   { travelName: "Uzebequistan", rnavtRef: "RNAVT 1234", imgSrc: UzebequistanCardImage, price: 413 },
 ];
+
+const countries = [
+  {
+    value: "madrid",
+    name: "Madrid",
+  },
+  {
+    value: "lisbon",
+    name: "Lisbon",
+  },
+  {
+    value: "london",
+    name: "London",
+  },
+  {
+    value: "paris",
+    name: "Paris",
+  },
+  {
+    value: "porto",
+    name: "Porto",
+  },
+];
+
+const passangers = [
+  { value: "1adults", name: "1 Adults, Economy" },
+  { value: "2adults", name: "2 Adults, Economy" },
+  { value: "1adults-1child", name: "1 Adults + 1 Child, Economy" },
+  { value: "2adults-2children", name: "2 Adults + 2 Children, Economy" },
+];
+
+interface FormValues {
+  travelDates: CalendarDate | Date;
+  departure: number | null;
+  arrival: number | null;
+}
 
 function HeroSection() {
   const heroSwiperRef = useRef<HTMLElement & { swiper: SwiperClass }>(null);
@@ -80,7 +122,10 @@ function HeroSection() {
   }, []);
 
   return (
-    <div className="flex w-full relative justify-center items-center " style={{ height: "650px" }}>
+    <div
+      className="flex w-full relative justify-center items-center "
+      style={{ minHeight: "650px" }}
+    >
       {/* Swiper */}
       <swiper-container
         ref={heroSwiperRef}
@@ -106,7 +151,7 @@ function HeroSection() {
       </swiper-container>
 
       {/* Cards */}
-      <div className="absolute lg:w-7/12 xl:w-8/12 h-4/6 z-10">
+      <div className="absolute lg:w-7/12 xl:w-8/12 min-h-96 z-10">
         <div className="w-full h-full">
           <div className="w-full h-full bg-white rounded-md py-4 px-6">
             <div className="">
@@ -141,6 +186,124 @@ function HeroSection() {
                   </swiper-slide>
                 ))}
               </swiper-container>
+            </div>
+
+            <div className="">
+              <h5 className="text-xl text-primary font-black">Search & Go</h5>
+              <hr />
+            </div>
+
+            <div className="my-5">
+              <div>
+                <Formik
+                  initialValues={{
+                    travelDates: today(getLocalTimeZone()),
+                    departure: null,
+                    arrival: null,
+                  }}
+                  onSubmit={(values: FormValues, { setSubmitting }: FormikHelpers<FormValues>) => {
+                    // setTimeout(() => {
+                    //   alert(JSON.stringify(values, null, 2));
+                    //   setSubmitting(false);
+                    // }, 500);
+
+                    const formData = {
+                      travelDates: values.travelDates,
+                      departure: values.departure
+                        ? countries[Number(values.departure)].value
+                        : null,
+                      arrival: values.arrival ? countries[Number(values.arrival)].value : null,
+                    };
+
+                    console.log({ values, c1: values.departure, c2: values.arrival, formData });
+                  }}
+                >
+                  {({ setFieldValue, values }) => (
+                    <Form className="flex flex-row gap-2">
+                      <Field name="travelDates">
+                        {({ field }) => (
+                          <DateRangePicker
+                            size="sm"
+                            className="xl:w-2/6"
+                            label="Choose dates"
+                            visibleMonths={2}
+                            pageBehavior="single"
+                            minValue={today(getLocalTimeZone())}
+                            defaultValue={today(getLocalTimeZone())}
+                            onChange={(travelDates) => setFieldValue("travelDates", travelDates)}
+                            isRequired
+                            {...field}
+                          />
+                        )}
+                      </Field>
+
+                      <Field name="departure">
+                        {({ field }) => (
+                          <Select
+                            size={"sm"}
+                            label="Departure"
+                            className="max-w-xs xl:w-1/6"
+                            onChange={(departure) => setFieldValue("departure", departure)}
+                            isRequired
+                            {...field}
+                          >
+                            {countries.map((country, key) => (
+                              <SelectItem key={key} value={country.value}>
+                                {country.name}
+                              </SelectItem>
+                            ))}
+                          </Select>
+                        )}
+                      </Field>
+
+                      <Field name="arrival">
+                        {({ field }) => (
+                          <Select
+                            size={"sm"}
+                            label="Arrival"
+                            className="max-w-xs xl:w-1/6"
+                            onChange={(arrival) => setFieldValue("arrival", arrival)}
+                            isRequired
+                            {...field}
+                          >
+                            {countries.map((country, key) => (
+                              <SelectItem key={key} value={country.value}>
+                                {country.name}
+                              </SelectItem>
+                            ))}
+                          </Select>
+                        )}
+                      </Field>
+
+                      <Field name="arrival">
+                        {({ field }) => (
+                          <Select
+                            size={"sm"}
+                            label="Passangers"
+                            className="max-w-xs xl:w-1/6"
+                            isRequired
+                          >
+                            {passangers.map((passanger, key) => (
+                              <SelectItem key={key} value={passanger.value}>
+                                {passanger.name}
+                              </SelectItem>
+                            ))}
+                          </Select>
+                        )}
+                      </Field>
+
+                      <Button
+                        type="submit"
+                        color="primary"
+                        className="xl:w-1/6 rounded-md text-white"
+                        size="lg"
+                      >
+                        Search
+                      </Button>
+                    </Form>
+                  )}
+                </Formik>
+              </div>
             </div>
           </div>
         </div>
